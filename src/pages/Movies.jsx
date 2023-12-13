aa6fa1b119c9f40a1c1d817b5201ca8a1e1a2abf
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-12-13 21:28:29 +0200

    qw3r2

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
   const [movies, setMovies] = useState([
@@ -10,30 +10,39 @@ const Movies = () => {
     'movie-50',
   ]);
 
+  const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
-  const movieId = searchParams.get('movieId');
+  const movieId = searchParams.get('movieId') ?? '';
 
   // useEffect(() => {
-  //   Пошук фільму за ключовим словом
+  // Пошук фільму за ключовим словом
 
-  // }, [])
+  // }, []);
+
+  const updateQueryString = event => {
+    const movieIdValue = event.target.value;
+    if (movieIdValue === '') {
+      return setSearchParams({});
+    }
+    setSearchParams({ movieId: movieIdValue });
+    // const nextParams = name !== '' ? { name } : {};
+    // setSearchParams(nextParams);
+  };
+
   const visibleMovies = movies.filter(movie => movie.includes(movieId));
 
+  // console.log('location', location);
   return (
     <>
       <h2>Пошук фільму за ключовим словом на сторінці фільмів.</h2>
-      <input
-        type="text"
-        value={movieId}
-        onChange={event => setSearchParams({ movieId: event.target.value })}
-      />
-      <button onClick={() => setSearchParams({ c: 'hello' })}>change</button>
+      <input type="text" value={movieId} onChange={updateQueryString} />
+      <button onClick={() => setSearchParams({ movieId })}>change</button>
       <div>
-        {movies.map(movie => {
+        {visibleMovies.map(movie => {
           return (
             <ul>
               <li>
-                <Link kay={movie} to={`${movie}`}>
+                <Link kay={movie} state={{ from: location }} to={`${movie}`}>
                   {movie}
                 </Link>
               </li>
