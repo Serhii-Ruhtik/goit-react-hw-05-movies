05cdafd30c18b7e23b9da5c822e507a541a01b38
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-12-16 10:43:05 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,17 +26,6 @@ const App = () => {
         </Route>
       </Routes>
     </Suspense>
-    // <Routes>
-    //   <Route path="/" element={<SharedLayout />}>
-    //     <Route index element={<Home />} />
-    //     <Route path="movies" element={<Movies />} />
-    //     <Route path="movies/:movieId" element={<MovieDetails />}>
-    //       <Route path="cast" element={<Cast />} />
-    //       <Route path="reviews" element={<Reviews />} />
-    //     </Route>
-    //     <Route path="*" element={<Navigate to="/" replace />} />
-    //   </Route>
-    // </Routes>
   );
 };
 
